3ebdce29098eb6e16fc8bdd2e119e87a7b8a7d35
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-06-14 09:55:29 -0300

    Create ex1.py

---
+++ Exercícios/Capítulo 4/ex1.py
@@ -0,0 +1,8 @@
+'''Crie um programa que receba um número do usuário e printe false quando o número for par e true quando ímpar.
+   Resolução:'''
+
+numero = int(input("Digite um número: "))
+
+impar = (numero % 2) != 0
+
+print("O número é ímpar? ", impar)
