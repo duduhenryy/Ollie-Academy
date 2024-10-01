b7747e94b9726c887d5e297dc9efb5813a6da718
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-01 16:09:54 -0300

    Update ex3.py

--- Exercícios/Capítulo 4/ex3.py
+++ Exercícios/Capítulo 4/ex3.py
@@ -1,23 +1,13 @@
-''' PA1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
-    PA2 = [5, 3, 1, -1, -3, -5, -7, -9, -11, -13]
-    PA3 = [20, 24, 28, 32, 36, 40, 44, 48, 52, 56]
-
-Usando como teste as três PA’s a cima para teste, crie um programa que peça ao usuário a posição do termo que ele deseja
-descobrir e os dois primeiros termos de uma PA de primeira ordem qualquer e printe na tela o termo que o usuário 
-escolheu descobrir.
-
-Ex:
-Se  o usuário escolher o termo na posição 10 e em seguida informar que os dois primeiros termos são 1 e 4 em sequência,
-o programa deve printar 28 como o décimo termo.
+''' Escreva um código que peça ao usuário dois números e, em seguida, adicione o segundo número ao primeiro três vezes consecutivas, 
+    usando o operador de atribuição adequado. Exiba o valor final.
 
 Resolução: '''
 
-posicao = int(input("Digite o termo que deseja descobrir: "))
 primeiro_termo = int(input("Digite o primeiro termo: "))
 segundo_termo = int(input("Digite o segundo termo: "))
 
-razao = segundo_termo - primeiro_termo
+primeiro_termo += segundo_termo
+primeiro_termo += segundo_termo
+primeiro_termo += segundo_termo
 
-decimo_termo = primeiro_termo + (posicao - 1) * razao
-
-print("O décimo termo da progressão aritmética é:", decimo_termo)
+print("O valor final é: ", primeiro_termo)
