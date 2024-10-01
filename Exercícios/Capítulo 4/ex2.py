7622f6aff217dd06f1bb2a4f8c7276ef481304a4
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-01 16:06:45 -0300

    Update ex2.py

--- Exercícios/Capítulo 4/ex2.py
+++ Exercícios/Capítulo 4/ex2.py
@@ -1,9 +1,6 @@
-'''Peça dois números ao usuário e printe se o segundo número é divisível pelo primeiro.
-   Resolução:'''
+'''Crie uma implementação que peça o valor de uma venda e calcule uma comissão de 5% sobre o valor. 
+   Use o operador de atribuição adequado para isso. Exiba o valor da comissão.'''
 
-numero1 = int(input("Digite um número: "))
-numero2 = int(input("Digite outro número: "))
-
-divisivel = numero2 % numero1 == 0
-
-print(numero2, "é divisivel por", numero1, "?", divisivel)
+valor = int(input("Digite o valor da venda: "))
+valor *= 0.05
+print("O valor da comissao é de: ", valor)
