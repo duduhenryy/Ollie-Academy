600965ffce6ffeb3ce8b20abc535de8c6d09ce07
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-01 16:34:20 -0300

    Create ex6.py

---
+++ Exercícios/Capítulo 4/ex6.py
@@ -0,0 +1,5 @@
+'''Peça ao usuário um número e divida-o por 2 usando o operador de atribuição adequado. Exiba o resultado final.'''
+
+valor = int(input("Digite o valor a ser dividido: "))
+valor /= 2
+print("A metade do valor é ", valor)
