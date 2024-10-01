3354bde1f6d0dd708d7aef825ebba47c1708f5ec
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-01 16:06:57 -0300

    Update ex1.py

--- Exercícios/Capítulo 4/ex1.py
+++ Exercícios/Capítulo 4/ex1.py
@@ -2,7 +2,5 @@
 usando o operador de atribuição adequado. Exiba o preço final com desconto.'''
 
 preco = int(input("Digite o valor do produto: "))
-
 preco *= 0.9
-
 print("O valor do produto com desconto de 10% é: ", preco)
