4d5da003fa47fd168e58884a69dfdea6ed2f2e95
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-01 16:28:10 -0300

    Update ex5.py

--- Exercícios/Capítulo 4/ex5.py
+++ Exercícios/Capítulo 4/ex5.py
@@ -1,1 +1,7 @@
+'''Crie um programa que peça ao usuário o tempo inicial de uma viagem (em horas). 
+Depois, aumente o tempo da viagem em 3 horas usando o operador de atribuição adequado. Exiba o tempo total de viagem.'''
 
+tempo = int(input("Digite o tempo de viagem: "))
+
+tempo += 3
+print("o tempo da viagem com o atraso é de ", tempo)
