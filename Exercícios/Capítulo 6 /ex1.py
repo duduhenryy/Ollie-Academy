ffe9217b0935af31758d3bc859b1a23396f0c0ac
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2024-10-19 10:19:02 -0300

    Create ex1.py

---
+++ Exercícios/Capítulo 6 /ex1.py
@@ -0,0 +1,10 @@
+#Utilizando o loop ‘for’, faça um programa que imprima todos os números pares de 1 a 20. Depois, faça outro código que imprima os ímpares.
+print("---------- PARES ------------")
+
+for i in range(2, 21, 2):
+    print(i)
+
+print("---------- ÍMPARES ------------")
+
+for j in range(1, 21,2):
+    print(j)
