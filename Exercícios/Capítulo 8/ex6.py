8bb54f453ca16261edf4e3afbed06c47b44306f5
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-01-07 09:42:55 -0300

    Create ex6.py

---
+++ Exercícios/Capítulo 8/ex6.py
@@ -0,0 +1,5 @@
+dados = ("João", 25, "Engenharia")
+nome, idade, curso = dados        # Desempacota os valores
+print(nome)                       # Saída: João
+print(idade)                      # Saída: 25
+print(curso)                      # Saída: Engenharia
