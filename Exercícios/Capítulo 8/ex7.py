5ca390eef3d76cc1de336bf4a1c120424fdeb294
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-01-07 09:43:38 -0300

    Create ex7.py

---
+++ Exercícios/Capítulo 8/ex7.py
@@ -0,0 +1,10 @@
+alunos = [
+    {"nome": "Ana", "idade": 20, "nota": 8.5},
+    {"nome": "Pedro", "idade": 22, "nota": 7.0}
+]
+
+for aluno in alunos:
+    print(f"Nome: {aluno['nome']}, Nota: {aluno['nota']}")
+# Saída:
+# Nome: Ana, Nota: 8.5
+# Nome: Pedro, Nota: 7.0
