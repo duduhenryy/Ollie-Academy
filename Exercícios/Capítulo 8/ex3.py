b6470aae75a242e3d8e59439c324a57f7d80892b
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-01-07 09:40:22 -0300

    Create ex3.py

---
+++ Exercícios/Capítulo 8/ex3.py
@@ -0,0 +1,6 @@
+numeros = [10, 20, 30, 40, 50]
+numeros[2] = 35                   # Substitui 30 por 35
+numeros.append(60)                # Adiciona 60
+numeros.remove(10)                # Remove 10
+print(len(numeros))               # Saída: 5
+print(numeros)                    # Saída: [20, 35, 40, 50, 60]
