2b9d3e9b5a6998d2d56a79c68ec61eff46842839
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-01-07 09:40:02 -0300

    Create ex2.py

---
+++ Exercícios/Capítulo 8/ex2.py
@@ -0,0 +1,4 @@
+numeros = list(range(1, 11))       # Cria uma lista com números de 1 a 10
+for numero in numeros:
+    if numero % 2 == 0:           # Verifica se o número é par
+        print(numero)             # Saída: 2, 4, 6, 8, 10
