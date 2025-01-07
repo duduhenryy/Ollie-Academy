b3288d4d20080d0fd92eb693ec33a68c668bda5a
Eduardo Henrique <105989448+duduhenryy@users.noreply.github.com>
2025-01-07 09:39:29 -0300

    Create ex1.py

---
+++ Exercícios/Capítulo 8/ex1.py
@@ -0,0 +1,5 @@
+compras = ["maçã", "banana", "pão"]
+compras.append("leite")           # Adiciona "leite"
+compras.remove("pão")             # Remove "pão"
+compras[1] = "laranja"            # Substitui "banana" por "laranja"
+print(compras)                    # Saída: ['maçã', 'laranja', 'leite']
